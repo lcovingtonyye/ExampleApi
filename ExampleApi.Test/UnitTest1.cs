f962611eb861fb84948db4a2c4af42ef4ed2905b
Leighton Covington <lcovington@yyesoftware.com>
2023-06-30 13:34:50 -0400

    Experiment with 2nd approach where we change what config values we build our host with;

--- ExampleApi.Test/UnitTest1.cs
+++ ExampleApi.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 
 
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using System.Net.Http.Json;
 
 namespace ExampleApi.Test
@@ -12,7 +14,28 @@ namespace ExampleApi.Test
         public async Task TestMethod1()
         {
             // Arrange
-            using var app = new WebApplicationFactory<Program>(); 
+            using var app = new WebApplicationFactory<Program>();
+            
+            var client = app.CreateClient(); // can pass in delegate handlers etc to configure the client (like set headers for auth etc)
+
+            // Act
+            var weather = await client.GetFromJsonAsync<WeatherForecast>("/WeatherForecast");
+
+            // Assert
+            Assert.IsNotNull(weather);
+            Assert.IsTrue(weather.TemperatureC == 50);
+            Assert.IsTrue(weather.Summary == "Scorching");
+        }
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            // Arrange
+            using var app = new WebApplicationFactory<Program>().WithWebHostBuilder((IWebHostBuilder webHostBuilder) =>
+            {
+                webHostBuilder.ConfigureAppConfiguration(x => x.AddCommandLine(new string[] { "testarg" }));
+            });
+
             var client = app.CreateClient(); // can pass in delegate handlers etc to configure the client (like set headers for auth etc)
 
             // Act
